09ba3b4cfaf6c8682d398764ec37807c21763ca3
MatiasEM12 <matiasemimellado@gmail.com>
2026-03-15 12:01:39 -0300

    Refactor de Plato y Bebida con una clase padre

--- src/main/java/Entities/Pedido.java
+++ src/main/java/Entities/Pedido.java
@@ -10,16 +10,58 @@ public class Pedido {
 
     private ArrayList<Item> items;
     private Boolean confirmado;
-    private Double propina;
+    private Integer propina;
     private Tarjeta tarjeta;
 
-    public Pedido(ArrayList<Item> items, Tarjeta tarjeta) {
+    public Pedido(ArrayList<Item> items, Tarjeta tarjeta, Integer propina, Boolean confirmado) {
+
         validarItems(items);
         validarTarjeta(tarjeta);
+        validarPropina(propina);
+        validarConfirmacion(confirmado );
 
         this.items = items;
-        this.confirmado = false;
-        this.propina = 0.0;
+        this.confirmado = confirmado;
+        this.propina = propina;
         this.tarjeta = tarjeta;
     }
+
+    public void confirmarPedido(){
+        if(items.isEmpty()){
+            throw new IllegalStateException("El pedido debe contener al menos un item para ser confirmado.");
+        }
+
+        if(confirmado) throw new IllegalStateException("El pedido ya ha sido confirmado.");
+
+        confirmado=true;
+    }
+
+    //VALIDACIONES
+
+    private void validarItems(ArrayList<Item> items){
+        if(items==null || items.isEmpty()){
+            throw new IllegalArgumentException("El pedido debe contener al menos un item.");
+        }
+    }
+
+    private void validarTarjeta(Tarjeta tarjeta){
+        if(tarjeta==null){
+            throw new IllegalArgumentException("El pedido debe tener una tarjeta asociada.");
+        }
+    }
+
+    private void validarPropina(Integer propina){
+        if(propina==null || (propina!=PROPINA_BAJA && propina!=PROPINA_MEDIA && propina!=PROPINA_ALTA)){
+            throw new IllegalArgumentException("La propina debe ser 2, 3 o 5.");
+        }
+
+    }
+
+    private void validarConfirmacion(Boolean confirmado){
+        if(confirmado==null){
+            throw new IllegalArgumentException("El estado de confirmación no puede ser nulo.");
+        }
+    }
+
+
 }
---
+++ src/main/java/Entities/Producto.java
@@ -0,0 +1,29 @@
+package Entities;
+
+public abstract  class Producto {
+
+    private String nombre;
+    private Double precio;
+
+    public Producto(String nombre, Double precio) {
+        validarNombre(nombre);
+        validarPrecio(precio);
+
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    //VALIDACIONES
+
+    private void validarNombre(String nombre) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre del producto no puede ser nulo o vacío.");
+        }
+    }
+
+    private void validarPrecio(Double precio) {
+        if (precio == null || precio < 0) {
+            throw new IllegalArgumentException("El precio del producto debe ser un número positivo.");
+        }
+    }
+}
