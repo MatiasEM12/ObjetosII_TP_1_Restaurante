041502de041a03f11213c158dac49dff0c9b2262
MatiasEM12 <matiasemimellado@gmail.com>
2026-03-18 18:44:39 -0300

    test

---
+++ src/test/java/TarjetasTest.java
@@ -0,0 +1,19 @@
+import Entities.Pedido;
+import Entities.Producto;
+import Entities.TarjetaCredito;
+import Entities.TarjetaVisa;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TarjetasTest {
+    @Test
+    public void testTarjetaCredito() {
+
+
+    }
+
+
+
+}
+
+
